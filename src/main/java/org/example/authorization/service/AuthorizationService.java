ed00c8c44bf34748646ec6dd00ae9fb7b1293cdf
VadimLushnikov <lushnikovvadim@yandex.ru>
2023-01-30 20:27:55 +0300

    Exception

--- src/main/java/org/example/authorization/service/AuthorizationService.java
+++ src/main/java/org/example/authorization/service/AuthorizationService.java
@@ -5,6 +5,8 @@ import org.example.authorization.User;
 import org.example.authorization.exceptions.InvalidCredentials;
 import org.example.authorization.exceptions.UnauthorizedUser;
 import org.example.authorization.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -27,10 +29,12 @@ public class AuthorizationService {
 //    }
     public List<Authorities> getAuthoritiesList(User user) {
         if (isEmpty(user.getUser()) || isEmpty(user.getPassword())) {
+            //throw new ResponseStatusException(HttpStatus.BAD_REQUEST,new InvalidCredentials("User name or password is empty").getMessage());
             throw new InvalidCredentials("User name or password is empty");
         }
         List<Authorities> userAuthorities = userRepository.getUserAuthorities(user.getUser(), user.getPassword());
         if (isEmpty(userAuthorities)) {
+            //throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,new InvalidCredentials("Unknown user " + user).getMessage());
             throw new UnauthorizedUser("Unknown user " + user);
         }
         System.out.println(userAuthorities+" userAuthorities");
